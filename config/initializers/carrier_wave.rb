e36742f6623f3861587dccfe89e516088129e8cb
Luke Allen <lukeallen@gmail.com>
2015-03-15 07:47:23 -0700

    fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,5 @@
-=begin
-if Rails.env.production?
+
+#if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
         # Configuration for Amazon S3
@@ -9,6 +9,5 @@ if Rails.env.production?
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
-end
+#end
 
-=end
