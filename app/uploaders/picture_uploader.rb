bdc3b9cf5a3d639284a3faba20de8c3351396519
Luke Allen <lukeallen@gmail.com>
2015-03-26 17:11:26 -0700

    pdfs upload and iframe only in message board

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,6 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
   #process resize_to_limit: [400, 400]
+  #process :set_content_type
 
   # Choose what kind of storage to use for this uploader:
   #if Rails.env.production?
