0c782c4871fd5172ee036d3d8a0891b1ff6e5664
Luke Allen <lukeallen@gmail.com>
2015-04-17 12:23:44 -0700

    fixed languages better

--- config/initializers/prefereneces.rb
+++ config/initializers/prefereneces.rb
@@ -1,4 +1,6 @@
-$available_languages = ['Universal (no language)','na'], ['English','en'], ['Mandarin','zh'], ['Hindi','hi'], ['Arabic','ar'], ['Spanish','es'], ['Portuguese','pt'], ['Bengali','bn']
+$available_languages = [ [ ['Universal (no language)','na'], ['English','en'], ['Mandarin','zh'], ['Hindi','hi'], ['Arabic','ar'], ['Spanish','es'], ['Portuguese','pt'], ['Bengali','bn'] ] ]
+$av_langs_hash = {'na' => 'Universal (no language)', 'en' => 'English','zh' => 'Mandarin','hi' => 'Hindi', 'ar' => 'Arabic', 'es' => 'Spanish', 'pt' => 'Portuguese', 'bn' => 'Bengali'  }
+
 $available_skills = 'Universal (skill level does not apply)', 'Basic', 'Intermediate', 'Advanced'
 $available_categories = 'Introduction / Main Content', 'I', 'II', 'III', 'Peak'
 
