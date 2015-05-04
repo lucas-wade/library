abcdd06cbf24301b77e3c456d25c41915936b9ce
Luke Allen <lukeallen@gmail.com>
2015-05-04 14:32:59 -0700

    superscript title fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,12 +18,12 @@ module ApplicationHelper
   end
 
   def header_title(mode = '')
-    base_title = t('titles.title')
+    base_title = t('titles.title_html')
 
     if mode.empty?
-      "#{base_title} #{t('titles.classroom')}"
+      "#{base_title} #{t('titles.classroom')}".html_safe
     else if mode == 'library'
-      "#{base_title}  #{t('titles.library')}"
+      "#{base_title}  #{t('titles.library')}".html_safe
          else if mode == 'clear'
            base_title
     end
