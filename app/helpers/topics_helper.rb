16c0a078068fec2e8721f6f10ca97aa8f8dd4c97
Luke Allen <lukeallen@gmail.com>
2015-05-06 10:52:15 -0700

    edit mode enabled only allows editing topics, no normal viewing.

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -6,7 +6,7 @@ module TopicsHelper
 
   def tab_active(t)
 
-     if t.main_content == nil || t.main_content == ''
+     if t.main_content == nil || t.main_content == '' && t.placeholder == FALSE && t.growing == FALSE
          the_class = 'tab-cat' + t.category.to_s + ' tab-greyed-out'
        else
          the_class = 'tab-cat' + t.category.to_s
