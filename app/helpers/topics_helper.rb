7780c2c556635e3bb11ba32f85f301eccf0f9491
Luke Allen <lukeallen@gmail.com>
2015-05-13 16:11:44 -0700

    bg color classroom doors

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -5,12 +5,10 @@ module TopicsHelper
   end
 
   def tab_active(t)
-
      if t.main_content == nil || t.main_content == '' && t.placeholder == FALSE && t.growing == FALSE
          the_class = 'tab-cat' + t.category.to_s + ' tab-greyed-out'
        else
          the_class = 'tab-cat' + t.category.to_s
-
      end
   end
 
@@ -19,6 +17,14 @@ module TopicsHelper
       rtl_class = 'rtl'
     else
       rtl_class = ''
+    end
+  end
+
+  def shot_topic_first_parent_or_self
+    if @topic.parents.first.present?
+      @topic.parents.first
+    else
+      @topic
     end
   end
 
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -139,4 +139,5 @@ class Topic < ActiveRecord::Base
   end
 
 
+
 end
