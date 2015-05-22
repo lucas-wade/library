c6632a535f026aa68dce2f510afc73b07bfc1a40
Luke Allen <lukeallen@gmail.com>
2015-05-22 13:41:32 -0700

    fixed modal partial

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,8 @@
-$('.btn-main-menu').click(function(){
-    //$('.v-bar').removeClass('v-current');
-    //$(this).addClass('v-current');
-    $('.column-1').addClass('fadeOutLeft')
-    $(this).preventDefault();
+$(document).ready(function() {
+    $('.btn-main-menu').click(function () {
+        //$('.v-bar').removeClass('v-current');
+        //$(this).addClass('v-current');
+        $('.column-1').addClass('animated fadeOutLeft')
+        //$(this).preventDefault();
+    });
 });
